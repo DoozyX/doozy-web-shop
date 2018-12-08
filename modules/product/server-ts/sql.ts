901e7fa5c0db6ac25401e54fc7724e3667345b83
Slobodan Kletnikov <slobodan_kletnikov@hotmail.com>
2018-12-08 20:43:43 +0100

    Add products, brand, category graphql queries

--- modules/product/server-ts/sql.ts
+++ modules/product/server-ts/sql.ts
@@ -1,27 +1,83 @@
 import knex from '../../../packages/server/src/sql/connector';
 
-export interface Product {
+export interface ProductType {
   id: number;
   name: string;
   rating: number;
   size: string;
   type: string;
+  categoryId: number;
+  brandId: number;
+}
+
+export interface BrandType {
+  id: number;
+  name: string;
+}
+
+export interface CategoryType {
+  id: number;
+  name: string;
 }
 
 export interface Identifier {
   id: number;
 }
 
-export default class ProductDAO {
+export class Product {
   public get(id: number) {
     return knex
-      .select('p.id', 'p.name', 'p.rating', 'p.size', 'p.type')
+      .select('p.id', 'p.name', 'p.rating', 'p.size', 'p.type', 'p.brandId', 'p.categoryId')
       .from('product as p')
       .where('p.id', '=', id)
       .first();
   }
 
+  public getAllForBrand(id: number) {
+    return knex
+      .select('p.id', 'p.name', 'p.rating', 'p.size', 'p.type', 'p.brandId', 'p.categoryId')
+      .from('product as p')
+      .where('p.brandId', '=', id);
+  }
+
+  public getAllForCategory(id: number) {
+    return knex
+      .select('p.id', 'p.name', 'p.rating', 'p.size', 'p.type', 'p.brandId', 'p.categoryId')
+      .from('product as p')
+      .where('p.categoryId', '=', id);
+  }
+
   public getAll() {
-    return knex.select('p.id', 'p.name', 'p.rating', 'p.size', 'p.type').from('product as p');
+    return knex
+      .select('p.id', 'p.name', 'p.rating', 'p.size', 'p.type', 'p.brandId', 'p.categoryId')
+      .from('product as p');
+  }
+}
+
+export class Brand {
+  public get(id: number) {
+    return knex
+      .select('b.id', 'b.name')
+      .from('brand as b')
+      .where('b.id', '=', id)
+      .first();
+  }
+
+  public getAll() {
+    return knex.select('b.id', 'b.name').from('brand as b');
+  }
+}
+
+export class Category {
+  public get(id: number) {
+    return knex
+      .select('c.id', 'c.name')
+      .from('category as c')
+      .where('c.id', '=', id)
+      .first();
+  }
+
+  public getAll() {
+    return knex.select('c.id', 'c.name').from('category as c');
   }
 }
--- packages/server/src/database/migrations/007_product.js
+++ packages/server/src/database/migrations/007_product.js
@@ -1,14 +1,44 @@
 export async function up(knex) {
-  return knex.schema.createTable('product', table => {
-    table.increments();
-    table.string('name').notNull();
-    table.string('type').notNull();
-    table.string('size').notNull();
-    table.integer('rating').notNull();
-    table.timestamps(false, true);
-  });
+  return Promise.all([
+    knex.schema
+      .createTable('category', table => {
+        table.increments();
+        table.string('name');
+        table.timestamps(false, true);
+      })
+      .createTable('brand', table => {
+        table.increments();
+        table.string('name');
+        table.timestamps(false, true);
+      })
+      .createTable('product', table => {
+        table.increments();
+        table.string('name').notNull();
+        table.string('type').notNull();
+        table.string('size').notNull();
+        table.integer('rating').notNull();
+        table
+          .integer('categoryId')
+          .unsigned()
+          .references('id')
+          .inTable('category')
+          .onDelete('CASCADE');
+        table
+          .integer('brandId')
+          .unsigned()
+          .references('id')
+          .inTable('brand')
+          .onDelete('CASCADE');
+        table.timestamps(false, true);
+      })
+  ]);
 }
 
 export async function down(knex) {
-  return knex.schema.dropTable('product');
+  return Promise.all([
+    knex.schema
+      .dropTable('product')
+      .dropTable('brand')
+      .dropTable('category')
+  ]);
 }
