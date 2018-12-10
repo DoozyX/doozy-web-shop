b4b0ea571f133971af0c8299da299e2094b0bb39
Slobodan Kletnikov <slobodan_kletnikov@hotmail.com>
2018-12-10 09:21:38 +0100

    Change app name

--- config/app.js
+++ config/app.js
@@ -1,5 +1,5 @@
 export default {
-  name: 'Apollo Starter Kit',
+  name: 'Doozy Web Shop',
   logging: {
     level: ['production'].indexOf(process.env.NODE_ENV) < 0 ? 'debug' : 'info',
     debugSQL: false,
