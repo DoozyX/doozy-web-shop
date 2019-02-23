9e11832d1a829fa1ae494b694473e2e322e5f46b
Doozy <slobodan_kletnikov@hotmail.com>
2019-02-23 21:44:06 +0100

    Refactor article views in single component

--- modules/article/client-react/containers/Article.tsx
+++ modules/article/client-react/containers/Article.tsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import Helmet from 'react-helmet';
+import moment from 'moment';
+import { useQuery } from 'react-apollo-hooks';
 import { RouteComponentProps } from 'react-router-dom';
+import { Spinner } from 'reactstrap';
 import { translate, TranslateFunction } from '@gqlapp/i18n-client-react';
-import ArticleView from '../components/ArticleView';
+
+import { PageLayout } from '@gqlapp/look-client-react';
+import settings from '../../../../settings';
+
+
+import GET_POST from '../graphql/GetPost.graphql';
 
 interface MatchParams {
   id: string;
 }
 
-interface ArticleProps extends RouteComponentProps<MatchParams> {
+interface ArticleViewProps extends RouteComponentProps<MatchParams> {
   t: TranslateFunction;
 }
 
-class Article extends React.Component<ArticleProps> {
-  public render() {
-    return <ArticleView {...this.props} />;
-  }
-}
+const renderMetaData = (t: TranslateFunction) => (
+  <Helmet
+    title={`${settings.app.name} - ${t('title')}`}
+    meta={[{ name: 'description', content: `${settings.app.name} - ${t('meta')}` }]}
+  />
+);
+
+const Article = ({ t, match }: ArticleViewProps) => {
+  const { data } = useQuery(GET_POST, { variables: { id: parseInt(match.params.id, 10) } });
+  return (
+    <PageLayout>
+      {renderMetaData(t)}
+      <Suspense fallback={<Spinner />}>
+        <div>
+          <div>
+            <h1>{data.post.title}</h1>
+            <small className="text-muted">{moment(parseInt(data.post.created_at, 10)).fromNow()}</small>
+          </div>
+          <img src={data.post.imageSource} alt="description" />
+          <p>{data.post.content}</p>
+        </div>
+      </Suspense>
+    </PageLayout>
+  );
+};
+
 
 export default translate('article')(Article);
--- modules/article/client-react/containers/Articles.tsx
+++ modules/article/client-react/containers/Articles.tsx
@@ -1,17 +1,88 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { translate, TranslateFunction } from '@gqlapp/i18n-client-react';
-import ArticlesView from '../components/ArticlesView';
+import Helmet from 'react-helmet';
+import moment from 'moment';
+import { Card, CardBody, CardTitle, CardText, Button, Spinner } from 'reactstrap';
 import { RouteComponentProps } from 'react-router-dom';
 
-interface ArticlesProps extends RouteComponentProps {
+import { PageLayout } from '@gqlapp/look-client-react';
+import settings from '../../../../settings';
+import { useQuery } from 'react-apollo-hooks';
+
+import GET_ALL_POSTS from '../graphql/GetAllPosts.graphql';
+
+interface ArticleViewProps extends RouteComponentProps {
   t: TranslateFunction;
 }
 
-class Articles extends React.Component<ArticlesProps> {
-  public render() {
-    return <ArticlesView {...this.props} />;
-  }
-}
+const renderMetaData = (t: TranslateFunction) => (
+  <Helmet
+    title={`${settings.app.name} - ${t('title')}`}
+    meta={[{ name: 'description', content: `${settings.app.name} - ${t('meta')}` }]}
+  />
+);
+
+const ArticleCard = ({
+  title,
+  description,
+  createdAt,
+  imageSource,
+  handleClick
+}: {
+  title: string;
+  description: string;
+  createdAt: any;
+  imageSource: string;
+  handleClick: () => void;
+}) => {
+  return (
+    <Card className="card flex-sm-row mb-gutter">
+      <CardBody width="80%">
+        <CardTitle>{title}</CardTitle>
+        <CardText
+          style={{
+            height: '160px',
+            'text-overflow': 'ellipsis',
+            overflow: 'hidden',
+            'white-space': 'pre-wrap;'
+          }}
+        >
+          {description}
+        </CardText>
+        <CardText>
+          <small className="text-muted">{moment(parseInt(createdAt, 10)).fromNow()}</small>
+        </CardText>
+        <Button onClick={handleClick}> View Article </Button>
+      </CardBody>
+      <img className="card-img-sm-right" width="20%!important" src={imageSource} alt="description" />
+    </Card>
+  );
+};
+
+const Articles = ({ t, history }: ArticleViewProps) => {
+  const { data } = useQuery(GET_ALL_POSTS, { suspend: true });
+  return (
+    <PageLayout>
+      {renderMetaData(t)}
+      <Suspense fallback={<Spinner />}>
+        <div>
+          {data.allPosts.map(
+            (x: { id: number; title: string; content: string; created_at: number; imageSource: string }) => (
+              <ArticleCard
+                key={x.id}
+                title={x.title}
+                description={x.content}
+                createdAt={x.created_at}
+                imageSource={x.imageSource}
+                handleClick={() => history.push(`/article/${x.id}`)}
+              />
+            )
+          )}
+        </div>
+      </Suspense>
+    </PageLayout>
+  );
+};
 
 export default translate('article')(Articles);
