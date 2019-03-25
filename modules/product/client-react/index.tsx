523ef36fbd13acd001adf82c30a06c92e969427e
Doozy <slobodan_kletnikov@hotmail.com>
2019-03-25 17:14:30 +0100

    Add farmer/customer nav links

--- modules/product/client-react/index.tsx
+++ modules/product/client-react/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import ClientModule from '@gqlapp/module-client-react';
-import { translate, TranslateFunction } from '@gqlapp/i18n-client-react';
+import { translate } from '@gqlapp/i18n-client-react';
 
 import { Route, NavLink } from 'react-router-dom';
 import { MenuItem } from '@gqlapp/look-client-react';
@@ -9,29 +9,40 @@ import Home from './Home';
 import Products from './Products';
 import Product from './Product';
 import resources from './locales';
+import { RootContext } from '@gqlapp/splash-screen-client-react';
 
-interface NavLinkProps {
-  text: string;
-  t: TranslateFunction;
-}
-
-const MenuItemWithI18n = translate('product')(({ t, text, ...rest }: NavLinkProps) => (
-  <MenuItem name={t(text)} as={NavLink} {...rest} />
-));
+const MenuItemWithI18n = translate('product')(({ t, text, farmerOnly = false, customerOnly = false, ...rest }: any) => {
+  const { farmer } = useContext(RootContext);
+  if (farmerOnly && !farmer) {
+    return null;
+  }
+  if (customerOnly && farmer) {
+    return null;
+  }
+  return <MenuItem name={t(text)} as={NavLink} {...rest} />;
+});
 
 export default new ClientModule({
   route: [
     <Route exact path="/" component={Home} />,
-    <Route exact path="/brands" component={Products} />,
     <Route exact path="/seeds" component={Products} />,
     <Route exact path="/products" component={Products} />,
-    <Route exact path="/product/:id" component={Product} />
+    <Route exact path="/product/new" component={Home} />,
+    <Route exact path="/product/:id" component={Product} />,
+    <Route exact path="/tools" component={Products} />,
+    <Route exact path="/fruits" component={Products} />,
+    <Route exact path="/vegetables" component={Products} />,
+    <Route exact path="/dairy" component={Products} />
   ],
   navItem: [
-    <MenuItemWithI18n key="/" text={'navLinkHome'} exact to="/" />,
-    <MenuItemWithI18n key="/brands" text={'navLinkBrands'} exact to="/brands" />,
-    <MenuItemWithI18n key="/products" text={'navLinkProducts'} exact to="/products" />,
-    <MenuItemWithI18n key="/seeds" text={'navLinkSeeds'} exact to="/seeds" />
+    <MenuItemWithI18n key="/" text={'navLink.home'} exact to="/" />,
+    <MenuItemWithI18n key="/products" text={'navLink.products'} exact to="/products" />,
+    <MenuItemWithI18n key="/seeds" text={'navLink.seeds'} exact farmerOnly to="/seeds" />,
+    <MenuItemWithI18n key="/tools" text={'navLink.tools'} exact farmerOnly to="/tools" />,
+    <MenuItemWithI18n key="/fruits" text={'navLink.fruits'} exact customerOnly to="/fruits" />,
+    <MenuItemWithI18n key="/vegetables" text={'navLink.vegetables'} exact customerOnly to="/vegetables" />,
+    <MenuItemWithI18n key="/dairy" text={'navLink.dairyProducts'} exact customerOnly to="/dairy" />
   ],
+  navItemRight: [<MenuItemWithI18n key="/product/new" text={'navLink.addProduct'} exact to="/product/new" />],
   localization: [{ ns: 'product', resources }]
 });
